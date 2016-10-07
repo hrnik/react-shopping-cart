6ac50ebec573c66e0ddbd8f6a283e9eec1fd45f2
Nikita Khristinin <nkhristinin@gmail.com>
2016-10-07 13:47:38 +1000

    [internal] add babel in webpack config, add ES2015, ES7, JSX

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,14 @@
 /**
  * Created by nkhristinin on 07.10.16.
  */
-var path = require('path')
-var webpack = require('webpack')
+var path = require('path');
+var webpack = require('webpack');
 
 module.exports = {
 	devtool: 'cheap-module-eval-source-map',
 	entry: [
 		'webpack-hot-middleware/client',
+		'babel-polyfill',
 		'./src/index'
 	],
 	output: {
@@ -19,5 +20,15 @@ module.exports = {
 		new webpack.optimize.OccurenceOrderPlugin(),
 		new webpack.HotModuleReplacementPlugin(),
 		new webpack.NoErrorsPlugin()
-	]
-}
+	],
+	module:{
+			loaders:[
+				{
+					loaders: ['babel-loader'],
+					include: [ path.resolve(__dirname, "src")],
+					test: /\.js$/,
+					plugins: ['transform-runtime']
+				}
+			]
+	}
+};
