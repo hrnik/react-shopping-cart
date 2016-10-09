8dd4b1348c002c1c4c368b607cad02b62502ef5d
Nikita Khristinin <nkhristinin@gmail.com>
2016-10-09 22:12:58 +1000

    [new] call method getProducts before initialize app, and add constant for GET_PRODUCTS actions

--- src/actions/ProductsAction.js
+++ src/actions/ProductsAction.js
@@ -1,22 +1,44 @@
-import { ADD_PRODUCT, DELETE_PRODUCT, REDUCE_PRODUCT_COUNT} from '../constants/Product';
+import API from '../api/API';
+import {
+	ADD_PRODUCT, DELETE_PRODUCT, REDUCE_PRODUCT_COUNT,
+	GET_PRODUCTS_FAILURE, GET_PRODUCTS_REQUEST, GET_PRODUCTS_SUCCESS
+} from '../constants/Product';
 
-export function addProduct(product) {
-	return {
+export const addProduct = product => ({
 		type: ADD_PRODUCT,
 		payload: product
-	}
+})
+
+export const deleteProduct = product => ({
+	type: DELETE_PRODUCT,
+	payload: product
+});
+
+export const reduceProductCount = product => ({
+	type: REDUCE_PRODUCT_COUNT,
+	payload: product
+});
+
+
+export const getProducts = id => dispatch => {
+	dispatch({
+		type: GET_PRODUCTS_REQUEST,
+		payload: id
+	});
+
+	API.getProducts().then(
+		product=> {
+			dispatch({
+				type: GET_PRODUCTS_SUCCESS,
+				payload: product
+			})
+		},
+		error=> {
+			dispatch({
+				type: GET_PRODUCTS_FAILURE,
+				payload: error
+			})
+		});
 }
 
-export function deleteProduct(product) {
-	return {
-		type: DELETE_PRODUCT,
-		payload: product
-	}
-}
 
-export function reduceProductCount(product) {
-	return {
-		type: REDUCE_PRODUCT_COUNT,
-		payload: product
-	}
-}
--- src/index.js
+++ src/index.js
@@ -3,9 +3,14 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import App from './containers/App';
-import configureStore from './store/configureStore'
+import configureStore from './store/configureStore';
+import {getProducts} from './actions/ProductsAction'
+
 
 const store = configureStore();
+
+//get all products for fake add btn's
+store.dispatch(getProducts());
 
 render(
 	<Provider store={store}>
